97f4d59c5ea9ba886548b2473f04da14989e28b4
JC <jcbcarbonel.ece@gmail.com>
2025-08-15 16:36:40 +0800

    chore: added source and header files

---
+++ dfrobot/Core/Inc/sen0290.h
@@ -0,0 +1,18 @@
+/*
+ * sen0290.h
+ *
+ *  Created on: Aug 15, 2025
+ *      Author: JohnCarloBCarbonel
+ */
+
+#ifndef INC_SEN0290_H_
+#define INC_SEN0290_H_
+
+// I2C address
+#define as3935_add1           0x01     ///< A0=high, A1=low
+#define as3935_add2           0x02     ///< A0=low, A1=high
+#define as3935_add3           0x03     ///< A0=high, A1=high
+
+
+
+#endif /* INC_SEN0290_H_ */
---
+++ dfrobot/Core/Src/sen0290.c
@@ -0,0 +1,10 @@
+/*
+ * sen0290.c
+ *
+ *  Created on: Aug 15, 2025
+ *      Author: JohnCarloBCarbonel
+ */
+
+#include <sen0290.h>
+
+
